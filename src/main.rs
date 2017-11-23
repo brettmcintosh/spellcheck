c2d60598c29b0f569dac16061b649ea4c1d80357
randy <whohat@gmail.com>
2017-11-22 21:35:38 -0800

    Add spelling recommendations

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,12 @@ struct TrieNode {
 }
 
 impl TrieNode {
-    fn create() -> TrieNode {
+    fn new() -> TrieNode {
         TrieNode { children: HashMap::with_capacity(26), is_word: false }
     }
 
     fn add_child(&mut self, chr: char) -> &mut TrieNode {
-        self.children.entry(chr).or_insert(TrieNode::create())
+        self.children.entry(chr).or_insert(TrieNode::new())
     }
 }
 
@@ -28,8 +28,8 @@ impl TrieNode {
 struct Trie { root: TrieNode }
 
 impl Trie {
-    fn create() -> Trie {
-        Trie { root: TrieNode::create() } }
+    fn new() -> Trie {
+        Trie { root: TrieNode::new() } }
 
     fn add_word(&mut self, word: &str) {
         let last = word.chars()
@@ -37,22 +37,48 @@ impl Trie {
         last.is_word = true;
     }
 
-    fn contains(&mut self, word: &str) -> bool {
-        match word.chars().fold(Some(&self.root), |maybe_node, char| {
+    fn contains(&self, word: &str) -> bool {
+        match self._get_prefix_node(&word) {
+            Some(v) => v.is_word,
+            None => false
+        }
+    }
+
+    fn get_words_with_prefix(&self, prefix: String) -> Vec<String> {
+        let mut words = Vec::new();
+        match self._get_prefix_node(&prefix) {
+            Some(v) => {
+                if v.is_word {
+                    words.push(prefix.clone());
+                }
+                let more_words: Vec<String> = v.children.keys()
+                    .map(|char| {
+                        let mut child_prefix = String::from(prefix.as_ref());
+                        child_prefix.push(char.clone());
+                        self.get_words_with_prefix(child_prefix)
+                    })
+                    .flat_map(|v| v)
+                    .collect();
+                words.extend(more_words.iter().cloned());
+            },
+            None => ()
+        }
+        words
+    }
+
+    fn _get_prefix_node(&self, prefix: &str) -> Option<&TrieNode> {
+        prefix.chars().fold(Some(&self.root), |maybe_node, char| {
             match maybe_node {
                 Some(v) => maybe_node.and(v.children.get(&char)),
                 None => None,
             }
-        }) {
-            Some(v) => v.is_word,
-            None => false
-        }
+        })
     }
 }
 
 impl From<File> for Trie {
     fn from(file: File) -> Self {
-        let mut trie = Trie::create();
+        let mut trie = Trie::new();
         let reader = BufReader::new(file);
         let mut num_words = 0;
         reader.lines()
@@ -65,47 +91,25 @@ impl From<File> for Trie {
     }
 }
 
-impl From<String> for Trie {
-    fn from(string: String) -> Self {
-        let mut trie = Trie::create();
-        let re = Regex::new(r"[[:^alpha:]]").unwrap();
-        string.split_whitespace()
-            // remove everything but letters
-            .map(|word| {
-                re.replace_all(word, "")
-            })
-            // make all lowercase
-            .map(|word| word.chars()
-                .map(|char| char.to_lowercase().last().unwrap())
-                .fold(String::new(), |mut s, c| {
-                    s.push(c);
-                    s
-                }))
-            .for_each(|word| trie.add_word(&word));
-        trie
-    }
-}
-
-#[allow(dead_code)]
 struct SpellChecker { word_set:Trie }
 
-#[allow(dead_code, unused_variables)]
 impl SpellChecker {
     fn new() -> SpellChecker {
-        let file = File::open(DICT_FILE).expect("File not found");
+        let file = File::open(DICT_FILE).expect("Could not read dictionary file");
         SpellChecker { word_set: Trie::from(file) }
     }
 
     fn is_word(&mut self, word: &str) -> bool {
-        self.word_set.contains(word)
+            self.word_set.contains(word)
     }
 
     fn check_string(&mut self, string: &str) -> Vec<String> {
+        // TODO consider also returning the word index in the string
         let re = Regex::new(r"[[:^alpha:]]").unwrap();
-        let misspelled_words: Vec<String> = string.split_whitespace()
+            string.trim().split_whitespace()
             // remove everything but letters
             .map(|word| {
-                re.replace_all(word, "")
+                    re.replace_all(word, "")
             })
             // make all lowercase
             .map(|word| word.chars()
@@ -113,31 +117,54 @@ impl SpellChecker {
                 .fold(String::new(), |mut s, c| {
                     s.push(c);
                     s
-                }))
+                })
+            )
             .filter(|word| !self.is_word(word))
-            .collect();
-        misspelled_words
+            .collect()
     }
 
-    fn get_recommendations(&self, word: &str, num: i16) { unimplemented!(); }
+    fn get_recommendations(&self, word: &str, num: usize) -> Vec<String> {
+        let mut all_recs: Vec<String> = (word.len()-2..word.len()+1)
+            .rev()
+            // get prefixes of len, len-1 and len-2
+            .map(|num| {
+                String::from(word.get(0..num).unwrap())
+            })
+            // recursively find words with prefix
+            .map(|prefix| self.word_set.get_words_with_prefix(prefix))
+            .flat_map(|v| v)
+            .take(num)  // some of these may be duplicates
+            .collect();
+        // remove duplicates
+        all_recs.sort_unstable();
+        all_recs.dedup();
+        all_recs
+    }
 }
 
 fn main() {
     let mut spell_checker = SpellChecker::new();
-    //    let test_words = [
-    //        "asdf", "yay", "coolest", "hotdog",
-    //        "pneumonia", "abracadabra",
-    //        "superabominableness",
-    //        "antidisestablishmentarianism",
-    //    ];
-    //    test_words.iter()
-    //        .for_each(|word| {
-    //            println!("Contains {}: {}", word, spell_checker.is_word(word));
-    //        });
+
     let test_string = "When using iterators, you'll often chain several \
     of them together. While working on such code, you might want to check \
     out what's happening at various parts in the pipeline. To do that, \
-    insert a call to inspect() stromesd mckurt doinhum murstlish";
+    insert a call to inspect() \
+    stromesd \
+    faithf \
+    razm \
+    crafed \
+    malark \
+    antidisestablishmentarianism";
+    println!("Misspelled words:");
     spell_checker.check_string(test_string).iter()
-        .for_each(|word| println!("{}", word))
+        .for_each(|word| {
+            println!("{}", word);
+            let recs = spell_checker.get_recommendations(word, 5);
+            if recs.len() > 0 {
+                println!("\tDid you mean: {:?}", recs);
+            }
+            else {
+                println!("\tNo recommendations for {}", word);
+            }
+        });
 }
